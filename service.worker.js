89f8706cda2e50868c89b686057b980254828be3
Mehryar <mehryartaghavi@gmail.com>
2026-05-02 21:02:30 +0330

    Update service.worker.js

--- service.worker.js
+++ service.worker.js
@@ -1,24 +1,35 @@
-const CACHE_NAME = "risk-app-v1";
+const CACHE_NAME = "risk-app-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
 ];
 
+// install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return Promise.all(
+          urlsToCache.map(url =>
+            cache.add(url).catch(err => {
+              console.log("Failed to cache:", url);
+            })
+          )
+        );
+      })
   );
 });
 
+// fetch (خیلی مهم)
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return response || fetch(event.request)
+          .catch(() => caches.match("/index.html"));
       })
   );
 });
